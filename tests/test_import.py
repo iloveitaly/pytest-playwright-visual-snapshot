9db1ea5395c49ccad68ce2130aa0c390586a4da3
Michael Bianco <mike@mikebian.co>
2025-03-21 16:10:41 -0600

    🎉 Initial commit

---
+++ tests/test_import.py
@@ -0,0 +1,8 @@
+"""Test pytest-playwright-visual-snapshot."""
+
+import pytest_playwright_visual_snapshot
+
+
+def test_import() -> None:
+    """Test that the  can be imported."""
+    assert isinstance(pytest_playwright_visual_snapshot.__name__, str)
